ebdf862d8b51771b1099535be283e29f2ea36048
Coburn Brandon <coburn.brandon@asurion.com>
2019-01-23 01:57:21 -0600

    Removed most of the content from Pokemon.js and hooks.js so we can start from almost scratch.

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,49 +1,14 @@
 import React from "react";
 import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
 import Grid from "@material-ui/core/Grid";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Typography from "@material-ui/core/Typography";
-import CircularProgress from "@material-ui/core/CircularProgress";
 
-import { imageViews } from "./constants";
-import { useFetch } from "./hooks";
-import { commaSeparate, catchPokemon } from './utils';
 import styles from './styles/Pokemon.styles';
 
-const Pokemon = ({ id }) => {
-  const pokeData = useFetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-
-  const pokemon = catchPokemon(pokeData);
-
+const Pokemon = () => {
   return (
     <Grid item xs={12} sm={6} md={4} lg={3}>
       <Card style={styles.card}>
-        {pokemon ? (
-          <CardActionArea>
-            <CardMedia
-              style={styles.media}
-              image={pokemon.images[imageViews[0]]}
-              title={pokemon.name}
-            />
-            <CardContent>
-              <Typography
-                align="center"
-                gutterBottom
-                variant="h5"
-                component="h2"
-              >
-                {pokemon.name}
-              </Typography>
-              <Typography align="center">{commaSeparate(pokemon.types)}</Typography>
-            </CardContent>
-          </CardActionArea>
-        ) : (
-          <div style={styles.loader}>
-            <CircularProgress style={styles.progress} />
-          </div>
-        )}
+
       </Card>
     </Grid>
   );
--- src/hooks.js
+++ src/hooks.js
@@ -1,22 +1,3 @@
-import { useState, useEffect } from "react";
-
-async function fetchData(url, setData) {
-  const response = await fetch(url);
-
-  const data = await response.json();
-
-  setData(data);
-}
-
 export function useFetch(url) {
-  const [data, setData] = useState({});
 
-  useEffect(
-    () => {
-      fetchData(url, setData);
-    },
-    [url]
-  );
-
-  return data;
 }
